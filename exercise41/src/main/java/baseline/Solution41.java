fbbb14b7ee8c1d3cd0b9889a8e9c3051ee363295
Udon-13 <maxllanes@gmail.com>
2021-10-15 21:32:34 -0400

    added UML and pseudocode for ex41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,27 @@
+package baseline;
+
+public class Solution41 {
+
+    /*
+    method: getNames
+        read by line and assign to string
+        store each data in array
+        return the string array
+     */
+
+    /*
+    method: organizeNames
+        use string compare function
+        if x is less
+        goes lower in pos
+        else
+        goes up in pos
+     */
+
+    public static void main(String[] args) {
+    /*
+    init files and other objects
+    call functions
+     */
+    }
+}
